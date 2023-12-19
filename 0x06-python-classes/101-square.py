0646aa817db6a1408bd6c90c19d5cb70b4378ad8
Jayharlem254 <jothamkilimo321@gmail.com>
2023-12-19 04:53:28 -0800

    task 101

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Module documentation for Square class"""
+
+
+class Square:
+    """Class to define a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Instantiation with optional size and optional position"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set position"""
+        if not isinstance(value, tuple) or len(value) != 2 or \
+                not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """String representation of the square"""
+        result = ""
+        if self.__size == 0:
+            return result
+
+        for _ in range(self.__position[1]):
+            result += "\n"
+
+        for _ in range(self.__size):
+            result += " " * self.__position[0] + "#" * self.__size + "\n"
+
+        return result.rstrip('\n')
